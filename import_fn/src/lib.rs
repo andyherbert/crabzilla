fb382ec06223e631d40e9b51e2d8b7aab217f1b1
Andy Herbert <andy.herbert@gmail.com>
2021-02-18 10:02:46 +0000

    Add meaningful error messages for import_fn macro

--- import_fn/src/lib.rs
+++ import_fn/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, ReturnType, Ident, Block};
-use quote::quote;
+use syn::{Block, Ident, parse_macro_input, spanned::Spanned};
+use quote::{quote, quote_spanned, ToTokens};
 
-fn quote_without_return(ident: &Ident, block: &Box<Block>, name: String) -> TokenStream {
+fn quote_without_return(ident: &Ident, block: &Box<Block>) -> TokenStream {
+    let name = ident.to_string();
     let result = quote! {
         fn #ident() -> crabzilla::ImportedFn {
             crabzilla::create_sync_fn(
@@ -16,7 +17,8 @@ fn quote_without_return(ident: &Ident, block: &Box<Block>, name: String) -> Toke
     result.into()
 }
 
-fn quote_with_return(ident: &Ident, block: &Box<Block>, name: String) -> TokenStream {
+fn quote_with_return(ident: &Ident, block: &Box<Block>) -> TokenStream {
+    let name = ident.to_string();
     let result = quote! {
         fn #ident() -> crabzilla::ImportedFn {
             crabzilla::create_sync_fn(
@@ -31,21 +33,41 @@ fn quote_with_return(ident: &Ident, block: &Box<Block>, name: String) -> TokenSt
     result.into()
 }
 
+fn error<T: Spanned>(item: T, msg: &str) -> TokenStream {
+    let span = item.span();
+    let error = quote_spanned! {
+        span => compile_error!(#msg);
+    };
+    error.into()
+}
+
 /// An attribute macro to convert Rust functions so they can be imported into a runtime.
 #[proc_macro_attribute]
 pub fn import_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as syn::ItemFn);
-    let sig = &input.sig;
-    let ident = &sig.ident;
-    let name = ident.to_string();
-    let block = &input.block;
+    match input.sig.inputs.to_token_stream().to_string().as_str() {
+        "" |
+        "args : Vec < Value >" |
+        "args : Vec < crabzilla :: Value >" => {},
+        "args : std :: vec :: Vec < Value >" => {},
+        "args : std :: vec :: Vec < crabzilla :: Value >" => {},
+        _ => return error(
+            input.sig.inputs,
+            "Illegal arguments, should be empty or \"args: Vec<Value>\""
+        ),
+    }
     match input.sig.asyncness {
         Some(_) => todo!(),
         None => {
-            if matches!(input.sig.output, ReturnType::Default) {
-                quote_with_return(ident, block, name)
-            } else {
-                quote_without_return(ident, block, name)
+            match input.sig.output.to_token_stream().to_string().as_str() {
+                "-> crabzilla :: Value" | "-> Value"
+                    => quote_without_return(&input.sig.ident, &input.block),
+                "-> ()" | ""
+                    => quote_with_return(&input.sig.ident, &input.block),
+                _ => error(
+                    input.sig.output,
+                    "Illegal return type, should be empty or \"Value\""
+                ),
             }
         }
     }
